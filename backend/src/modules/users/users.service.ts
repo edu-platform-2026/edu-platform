bb248516d2cbbef08135a39cb8fb01d359420a36
edu-platform-2026 <3410551089@qq.com>
2026-05-22 09:03:57 +0800

    fix: simplify status filter and fix bcrypt import in users.service

--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -5,7 +5,7 @@ import {
   BadRequestException,
   Logger,
 } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { PrismaService } from '../prisma/prisma.service';
 import { PaginationDto, createPaginatedResult } from '../../common/dto/pagination.dto';
 import { Role } from '../../common/enums/role.enum';
@@ -45,7 +45,7 @@ export class UsersService {
       where.institutionId = filters.institutionId;
     }
 
-    if (filters?.status != null && filters?.status !== '' && !isNaN(Number(filters.status))) {
+    if (filters?.status != null) {
       where.status = Number(filters.status);
     }
 
