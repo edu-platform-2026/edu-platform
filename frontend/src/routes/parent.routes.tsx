36f9ca3325250052bed30fff3f5833b9ad08bf3c
edu-platform-2026 <3410551089@qq.com>
2026-05-22 10:38:46 +0800

    sync: update frontend/src/routes/parent.routes.tsx

--- frontend/src/routes/parent.routes.tsx
+++ frontend/src/routes/parent.routes.tsx
@@ -5,6 +5,10 @@ import Dashboard from '../pages/parent/Dashboard';
 import Assignments from '../pages/parent/Assignments';
 import Progress from '../pages/parent/Progress';
 import Feedback from '../pages/parent/Feedback';
+import Profile from '../pages/common/Profile';
+import Messages from '../pages/common/Messages';
+import NotificationCenter from '../pages/common/NotificationCenter';
+import Invite from '../pages/common/Invite';
 
 const ParentRoutes: React.FC = () => {
   return (
@@ -14,6 +18,10 @@ const ParentRoutes: React.FC = () => {
         <Route path="assignments" element={<Assignments />} />
         <Route path="progress" element={<Progress />} />
         <Route path="feedback" element={<Feedback />} />
+        <Route path="messages" element={<Messages />} />
+        <Route path="notifications-center" element={<NotificationCenter />} />
+        <Route path="invite" element={<Invite />} />
+        <Route path="profile" element={<Profile />} />
         <Route path="*" element={<Navigate to="dashboard" replace />} />
       </Routes>
     </ParentLayout>
